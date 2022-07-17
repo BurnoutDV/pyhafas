e996669bd58e09cce9450a17d3d1809007bbc0d1
BurnoutDV <burnoutdv@gmail.com>
2022-07-17 23:28:41 +0200

    [profile:NASA] fixed missing import statement for profile and adjusted example accordingly

--- example_nasa.py
+++ example_nasa.py
@@ -2,7 +2,7 @@ import json
 import datetime
 
 from pyhafas import HafasClient
-from pyhafas.profile.nasa import NASAProfile
+from pyhafas.profile import NASAProfile
 
 client = HafasClient(NASAProfile())
 
